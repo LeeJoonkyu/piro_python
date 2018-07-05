4ce3bddcb498efa05c01e399f7d762fa6cae2080
Joonkyu <ljk6463@gmail.com>
2018-07-05 09:02:19 +0900

    cal_class

---
+++ cal_class.py
@@ -0,0 +1,15 @@
+def plus(num1, num2):
+    result = num1 + num2
+    return result
+
+def minus(num1, num2):
+    result = num1-num2
+    return result
+
+def multi(num1, num2):
+    result = num1*num2
+    return result
+
+def div(num1, num2):
+    result = num1/num2
+    return result
